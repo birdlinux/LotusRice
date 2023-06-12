bb43dadbfef41366d43edfccc7ddff1c92304003
George <123122904+birdlinux@users.noreply.github.com>
2023-06-12 12:50:43 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+print_ascii_art() {
+    echo "        _____  "
+    echo "      /  ___  \\"
+    echo "    /  /  _  \\  \\"
+    echo "  /( /( /(_)\ )\ )\\"
+    echo " (  \\  \\ ___ /  /  )"
+    echo " (    \\ _____ /    )"
+    echo " /(               )\\"
+    echo "|  \\             /  |"
+    echo "|    \\ _______ /    |"
+    echo " \\    / \\   / \\    /"
+    echo "   \\/    | |    \\/"
+    echo "         | | "
+    echo "         | |"
+    echo "         | |"
+    echo " "
+    echo " "
+}
+
+create_directories() {
+	echo "🢒 Creating the Folders"
+    pictures_directory="Pictures"
+    documents_directory="Documents"
+    picture_subdirectories=("ProfilePictures" "Screenshots" "Memes" "Important" "Junk")
+    document_subdirectories=("Books" "Projects" "Work Related")
+
+    for subdirectory in "${picture_subdirectories[@]}"; do
+        path="$pictures_directory/$subdirectory"
+        mkdir -p "$path"
+        echo "Created directory: $path"
+    done
+
+    for subdirectory in "${document_subdirectories[@]}"; do
+        path="$documents_directory/$subdirectory"
+        mkdir -p "$path"
+        echo "Created directory: $path"
+    done
+}
+
+update_system() {
+	echo "🢒 Updating the system"
+	sudo apt-get update -y &> /dev/null
+	sudo apt-get upgrade -y &> /dev/null
+}
+
+install_components() {
+    echo "🢒 Installing Dependencies"
+    sudo apt-get install libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson libxcb-util-dev feh xdo rofi flameshot wget libx11-dev curl gpg git libxcb-randr0-dev libxcb-xtest0-dev libxcb-xinerama0-dev libxcb-shape0-dev libxcb-xkb-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev bspwm sxhkd zathura ranger polybar dunst build-essential ninja-build nemo thunar python3 python3-pip python3-setuptools alacritty mpv cmus neofetch bashtop vim apt-transport-https golang -y &> /dev/null
+}
+
+install_picom() {
+	"🢒 Installing Picom"
+	git clone --quiet https://github.com/yshui/picom
+	cd picom
+	meson setup --buildtype=release . build
+	ninja -C build
+	cd build
+	cd src
+	chmod +x picom
+	cp picom /usr/bin/ 
+}
+
+install_fonts() {
+	echo "🢒 Installing and Configuring Fonts"
+    wget -q https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip
+    sudo unzip /tmp/shardic/JetBrainsMono.zip -d /usr/share/fonts &> /dev/null
+}
+
+setup_config() {
+	git clone --quiet https://github.com/birdlinux/LotusRice
+    cd ~/LotusRice/.config/
+    
+    echo "🢒 Configuring files"
+    cp -r * ~/.config/
+    
+    echo "🢒 Configuring scripts"
+    cd ~/LotusRice/
+    cp -r .scripts ~/
+    
+    echo "🢒 Setting permissions"
+    cd ~/.config/
+    sudo chmod +x * &> /dev/null
+    sudo chmod +x */* &> /dev/null
+    sudo chmod +x */*/* &> /dev/null
+    sudo chmod +x */*/*/* &> /dev/null
+    sudo chmod +x */*/*/*/* &> /dev/null
+    sudo chmod +x */*/*/*/*/* &> /dev/null
+    sudo chmod +x */*/*/*/*/*/* &> /dev/null
+}
+ 
+clean_files () {
+	cd ~
+	rm -rf JetBrainsMono.zip
+	rm -rf LotusRice
+	echo ""
+	echo "🢒 Restart your system for the changes to apply"
+	echo "🢒 Make sure to choose the BSPWM Window Manager on the Login Screen"
+}
+ 
+
+print_ascii_art
+create_directories
+update_system
+install_components
+install_picom
+install_fonts
+setup_config
+clean_files
+
+
+
